0b4a6c29fcc08e65b15e641b7bcdcf435f972f16
Cjtrankz <flyingapple55@gmail.com>
2023-03-06 17:04:05 -0500

    throw shuriken script

--- Assets/Scripts/ShurikenThrow.cs
+++ Assets/Scripts/ShurikenThrow.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class ShurikenThrow : MonoBehaviour
 {
 
+    Rigidbody rb;
+
     //lock x and y rotation upon throw
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        rb.velocity = new Vector3(0,0,0);
+        rb.isKinematic = true;
+    }
+
+    public void Throw(int num)
+    {
+        rb.AddForce(transform.forward*num, ForceMode.Impulse);
+        this.transform.Rotate(90,0,0);
     }
 }
