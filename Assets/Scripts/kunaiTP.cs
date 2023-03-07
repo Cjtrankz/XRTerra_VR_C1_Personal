ccce1f5f9c686bf64441f49259daa9bce5beb07d
Cjtrankz <flyingapple55@gmail.com>
2023-03-07 18:19:23 -0500

    finish kunai teleport

---
+++ Assets/Scripts/kunaiTP.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kunaiTP : MonoBehaviour
+{   
+    public GameObject body;
+    public GameObject kunai;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("clap");
+        if(col.gameObject.tag == "hands")
+        {
+            kunai = GameObject.Find("Kunai(Clone)");
+            if(kunai != null)
+            {
+                body.transform.position = new Vector3(kunai.transform.position.x, 0, kunai.transform.position.z);
+                Destroy(kunai, 2);
+            }
+        }
+    }
+
+}
